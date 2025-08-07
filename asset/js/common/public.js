14d7d15c52fcb6b8680978a59ab9d72e869fc46b
CHAEWOON <chaewoon0@gmail.com>
2025-08-07 14:44:47 +0900

    0807

--- asset/js/common/public.js
+++ asset/js/common/public.js
@@ -1,36 +1,36 @@
 // s: 250416 top버튼
-$(document).ready(function(){
-	"use strict";
-	var progressPath = document.querySelector('#topBtn .progress-circle path');
-	var pathLength = progressPath.getTotalLength();
-	progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-	progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-	progressPath.style.strokeDashoffset = pathLength;
-	progressPath.getBoundingClientRect();
-	progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-	var updateProgress = function () {
-		var scroll = $(window).scrollTop();
-		var height = $(document).height() - $(window).height();
-		var progress = pathLength - (scroll * pathLength / height);
-		progressPath.style.strokeDashoffset = progress;
-	}
-	updateProgress();
-	$(window).scroll(updateProgress);
-	var offset = 500;
-	var duration = 550;
-	jQuery(window).on('scroll', function() {
-		if (jQuery(this).scrollTop() > offset) {
-			jQuery('#topBtn').addClass('active-progress');
-		} else {
-			jQuery('#topBtn').removeClass('active-progress');
-		}
-	});
-	jQuery('#topBtn').on('click', function(event) {
-		event.preventDefault();
-		jQuery('html, body').animate({scrollTop: 0}, duration);
-		return false;
-	})
-});
+// $(document).ready(function(){
+// 	"use strict";
+// 	var progressPath = document.querySelector('#topBtn .progress-circle path');
+// 	var pathLength = progressPath.getTotalLength();
+// 	progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+// 	progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+// 	progressPath.style.strokeDashoffset = pathLength;
+// 	progressPath.getBoundingClientRect();
+// 	progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+// 	var updateProgress = function () {
+// 		var scroll = $(window).scrollTop();
+// 		var height = $(document).height() - $(window).height();
+// 		var progress = pathLength - (scroll * pathLength / height);
+// 		progressPath.style.strokeDashoffset = progress;
+// 	}
+// 	updateProgress();
+// 	$(window).scroll(updateProgress);
+// 	var offset = 500;
+// 	var duration = 550;
+// 	jQuery(window).on('scroll', function() {
+// 		if (jQuery(this).scrollTop() > offset) {
+// 			jQuery('#topBtn').addClass('active-progress');
+// 		} else {
+// 			jQuery('#topBtn').removeClass('active-progress');
+// 		}
+// 	});
+// 	jQuery('#topBtn').on('click', function(event) {
+// 		event.preventDefault();
+// 		jQuery('html, body').animate({scrollTop: 0}, duration);
+// 		return false;
+// 	})
+// });
 
 // e: 250416 top버튼
 	
